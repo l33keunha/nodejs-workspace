3e330e82ce48b64e8cf4e651e6a1ef5df066e404
l33keunha <l33keunh@gmail.com>
2022-10-24 11:28:11 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+let a = 15; // 변수
+const b = 45; // 상수
+
+// JSX 문법에서는 if문의 사용불가능하고, 대신 삼항연산자가 사용 가능
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>나도 드디어 노드js!!!!{a===20?"20":"20아냐"}{b}</div>
   );
 }
 
